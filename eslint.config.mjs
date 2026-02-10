721e710d14182fd5f1617ef6e2bddcdb69b9b038
montasirmoyen <montasirmoyen@gmail.com>
2026-02-10 15:01:16 -0500

    Update lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,18 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+// eslint-disable-next-line prefer-const
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    "rules": {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
